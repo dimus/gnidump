303449081941d2c6e2ab260f06491455c9b90467
Dmitry Mozzherin <dmozzherin@gmail.com>
2017-08-29 07:32:11 -0500

    all tables dumped

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,8 +15,16 @@ import (
 
 func main() {
 	db := setDb()
+	prepare()
 	DumpTables(db)
 	defer db.Close()
+}
+
+func prepare() {
+	gniPath := "/tmp/gni_mysql"
+	if _, err := os.Stat(gniPath); os.IsNotExist(err) {
+		os.Mkdir(gniPath, 0777)
+	}
 }
 
 func check(err error) {
@@ -44,9 +53,160 @@ func setDb() *sql.DB {
 
 func DumpTables(db *sql.DB) {
 	dumpTableDataSources(db)
+	dumpTableNameStrings(db)
+	dumpTableNameStringIndices(db)
+	dumpTableVernacularStrings(db)
+	dumpTableVernacularStringIndices(db)
+}
+
+func dumpTableVernacularStringIndices(db *sql.DB) {
+	log.Print("Create vernacular_string_indices.csv")
+	q := `SELECT data_source_id, taxon_id,
+					vernacular_string_id, language, locality,
+					country_code
+					FROM vernacular_string_indices`
+
+	handleVernacularStringIndices(RunQuery(db, q))
+}
+func handleVernacularStringIndices(rows *sql.Rows) {
+	var data_source_id, taxon_id, vernacular_string_id string
+	var language, locality, country_code sql.NullString
+	file := csvFile("vernacular_string_indices")
+	defer file.Close()
+	w := csv.NewWriter(file)
+	err := w.Write([]string{"data_source_id", "taxon_id", "vernacular_string_id",
+		"language", "locality", "country_code"})
+	check(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&data_source_id, &taxon_id, &vernacular_string_id,
+			&language, &locality, &country_code)
+		check(err)
+		csvRow := []string{data_source_id, taxon_id, vernacular_string_id,
+			language.String, locality.String, country_code.String}
+
+		if err := w.Write(csvRow); err != nil {
+			log.Fatal(err)
+		}
+	}
+	w.Flush()
+	file.Sync()
+}
+
+func dumpTableVernacularStrings(db *sql.DB) {
+	log.Print("Create vernacular_strings.csv")
+	q := "SELECT id, name FROM vernacular_strings"
+	handleVernacularStrings(RunQuery(db, q))
+}
+
+func handleVernacularStrings(rows *sql.Rows) {
+	var id string
+	var name string
+	file := csvFile("vernacular_strings")
+	defer file.Close()
+	w := csv.NewWriter(file)
+	err := w.Write([]string{"id", "name"})
+	check(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		check(err)
+		csvRow := []string{id, name}
+
+		if err := w.Write(csvRow); err != nil {
+			log.Fatal(err)
+		}
+	}
+	w.Flush()
+	file.Sync()
+}
+
+func dumpTableNameStringIndices(db *sql.DB) {
+	log.Print("Create name_string_indices.csv")
+	q := `SELECT data_source_id, name_string_id,
+					url, taxon_id, global_id, local_id,
+					nomenclatural_code_id, rank,
+					accepted_taxon_id, classification_path,
+					classification_path_ids,
+					classification_path_ranks
+					FROM name_string_indices`
+	handleNameStringIndices(RunQuery(db, q))
+}
+
+func handleNameStringIndices(rows *sql.Rows) {
+	var data_source_id, name_string_id, taxon_id string
+	var url, global_id, local_id, nomenclatural_code_id, rank sql.NullString
+	var accepted_taxon_id sql.NullString
+	var classification_path, classification_path_ids sql.NullString
+	var classification_path_ranks sql.NullString
+	file := csvFile("name_string_indices")
+	defer file.Close()
+	w := csv.NewWriter(file)
+	err := w.Write([]string{"data_source_id", "name_string_id", "url",
+		"taxon_id", "global_id", "local_id", "nomenclatural_code_id", "rank",
+		"accepted_taxon_id", "classification_path", "classification_path_ids",
+		"classification_path_ranks"})
+	check(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&data_source_id, &name_string_id, &url, &taxon_id,
+			&global_id, &local_id, &nomenclatural_code_id, &rank, &accepted_taxon_id,
+			&classification_path, &classification_path_ids,
+			&classification_path_ranks)
+		check(err)
+		csvRow := []string{data_source_id, name_string_id, url.String, taxon_id,
+			global_id.String, local_id.String, nomenclatural_code_id.String,
+			rank.String, accepted_taxon_id.String, classification_path.String,
+			classification_path_ids.String, classification_path_ranks.String}
+
+		if err := w.Write(csvRow); err != nil {
+			log.Fatal(err)
+		}
+	}
+	w.Flush()
+	file.Sync()
+}
+
+func dumpTableNameStrings(db *sql.DB) {
+	log.Print("Create name_strings.csv")
+	q := `SELECT id, name
+					FROM name_strings`
+	handleNameStrings(RunQuery(db, q))
+}
+
+func handleNameStrings(rows *sql.Rows) {
+	var id string
+	var name string
+	file := csvFile("name_strings")
+	defer file.Close()
+	w := csv.NewWriter(file)
+	err := w.Write([]string{"id", "name"})
+	check(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		check(err)
+		name := strings.Replace(name, "\u0000", "", -1)
+		csvRow := []string{id, name}
+
+		if err := w.Write(csvRow); err != nil {
+			log.Fatal(err)
+		}
+	}
+	w.Flush()
+	file.Sync()
 }
 
 func dumpTableDataSources(db *sql.DB) {
+	log.Print("Create data_sources.csv")
 	q := `SELECT id, title, description,
 	 	  		logo_url, web_site_url, data_url,
 	 	  		refresh_period_days, name_strings_count,
@@ -74,6 +234,13 @@ func handleDataSource(rows *sql.Rows) {
 	defer file.Close()
 	w := csv.NewWriter(file)
 
+	err := w.Write([]string{"id", "title", "description",
+		"logo_url", "web_site_url", "data_url",
+		"refresh_period_days", "name_strings_count",
+		"data_hash", "unique_names_count", "created_at",
+		"updated_at"})
+	check(err)
+
 	defer rows.Close()
 
 	for rows.Next() {
@@ -88,16 +255,14 @@ func handleDataSource(rows *sql.Rows) {
 			strconv.Itoa(int(unique_names_count.Int64)),
 			created_at.String(), updated_at.String()}
 
-		if err := w.Write(csvRow); err != nil {
-			log.Fatal(err)
-		}
+		check(w.Write(csvRow))
 	}
 	w.Flush()
 	file.Sync()
 }
 
 func csvFile(f string) *os.File {
-	file, err := os.Create("/tmp/" + f + ".csv")
+	file, err := os.Create("/tmp/gni_mysql/" + f + ".csv")
 	check(err)
 	return file
 }
